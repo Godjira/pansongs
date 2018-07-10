2e9bdbfc03f59b69b8a82792071b9832f097f6c1
Godjira <main.godjira@gmail.com>
2018-07-10 21:52:16 +0300

    Wip

--- PanSongs/PanSongs/Controllers/AddSongViewController.swift
+++ PanSongs/PanSongs/Controllers/AddSongViewController.swift
@@ -12,24 +12,57 @@ class AddSongViewController: UIViewController {
 
     @IBOutlet weak var chordsTextView: UITextView!
     @IBOutlet weak var textView: UITextView!
+    var frontText = true
 
     @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.bringSubview(toFront: chordsTextView)
+        scrollView.bringSubview(toFront: textView)
         chordsTextView.textColor = UIColor.orange
         textView.delegate = self
 
 
-        
+        initToolBarForKeyboard()
+        if textView.frame.height < scrollView.frame.height {
+            textView.frame = CGRect(origin: textView.frame.origin,
+                                    size: CGSize(width: textView.frame.width,
+                                                 height: scrollView.frame.height))
+        }
+    }
+
+    func initToolBarForKeyboard() {
+        //init toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 30))
+
+        //create left side empty space so that done button set on right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self,
+                                                       action: #selector(AddSongViewController.doneButtonAction))
+
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+
+        //setting toolbar as inputAccessoryView
+        self.chordsTextView.inputAccessoryView = toolbar
+        self.textView.inputAccessoryView = toolbar
+    }
+
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
 
     }
 
 
-
     @IBAction func textOrChordsAction(_ sender: UIButton) {
-        
+        if frontText {
+            scrollView.bringSubview(toFront: chordsTextView)
+            frontText = false
+        } else {
+            scrollView.bringSubview(toFront: textView)
+            frontText = true
+        }
     }
 }
 
@@ -37,7 +70,14 @@ extension AddSongViewController: UITextViewDelegate {
 
     func textViewDidChange(_ textView: UITextView) {
         if textView == self.textView {
+            //textView.frame = CGRect(origin: textView.frame.origin, size: CGSize(width: textView.frame.width, height: textView.contentSize.height))
             scrollView.contentSize.height = textView.frame.size.height
+        }
+
+        if textView.frame.height < scrollView.frame.height {
+            textView.frame = CGRect(origin: textView.frame.origin,
+                                    size: CGSize(width: textView.frame.width,
+                                                 height: scrollView.frame.height))
         }
     }
     
